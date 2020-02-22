13e3f845b1c29b40f14eda6f0ff5a1e1b7e0064f
Boris <boris.dryabin@gmail.com>
2020-02-22 17:41:29 +0300

    refactor

--- task6.php
+++ task6.php
@@ -4,21 +4,21 @@ echo '<table border="1px solid red">';
 
 for ($i = 0; $i <= 10; $i++) {
     echo '<tr><td>' . $i . ' </td>';
+
     for ($j = 0; $j <= 10; $j++) {
-        if ($i > 0) {
+        if ($i > 0):
             $multiplyRes = $i * $j;
-            if ($multiplyRes % 2 === 0) {
+            if ($multiplyRes % 2 === 0) :
                 echo '<td>(' . $multiplyRes . ')</td>';
-            } elseif ($multiplyRes % 2 !== 0) {
+            elseif ($multiplyRes % 2 !== 0) :
                 echo '<td>[' . $multiplyRes . ']</td>';
-            } else {
+            else :
                 echo '<td>' . $multiplyRes . '</td>';
-            }
-        } else {
+            endif;
+        else:
             echo '<td>' . $j . '</td>';
-        }
+        endif;
     }
     echo '</tr>';
 }
-
 echo '</table>';
