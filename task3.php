3e3e7aa3bd68f2c6de97d7f906134a353905a468
Boris <boris.dryabin@gmail.com>
2020-02-22 17:02:46 +0300

    add consts

--- task3.php
+++ task3.php
@@ -1,13 +1,17 @@
 <?php
+const MIN_AGE = 1;
+const MAX_AGE = 65;
+const ARMY_AGE = 18;
+const TEEN_AGE = 17;
 
 $age = 30;
 
 
-if ($age >= 18 && $age <= 65) {
+if ($age >= ARMY_AGE && $age <= MAX_AGE) {
     echo 'Вам еще работать и работать';
-} elseif ($age > 65) {
+} elseif ($age > MAX_AGE) {
     echo 'Вам пора на пенсию';
-} elseif ($age >= 1 && $age <= 17) {
+} elseif ($age >= MIN_AGE && $age <= TEEN_AGE) {
     echo 'Вам ещё рано работать';
 } else{
     echo 'Неизвестный возраст';
